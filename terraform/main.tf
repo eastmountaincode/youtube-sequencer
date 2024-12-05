58ad3f5d05572b6309dc695f59996f1b4dedca5c
Andrew Boylan <andrewboylan@Andrews-MacBook-Pro-M3.local>
2024-12-04 21:11:06 -0500

    run back to http

--- terraform/main.tf
+++ terraform/main.tf
@@ -110,6 +110,20 @@ resource "aws_security_group" "ec2_security_group" {
     cidr_blocks = ["0.0.0.0/0"]  # API access
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # HTTP access for Let's Encrypt
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # HTTP access for Let's Encrypt
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
