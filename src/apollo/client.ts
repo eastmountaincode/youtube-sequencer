3136ff192ec24580fd786d6d2293d6221d5a31f8
Andrew Boylan <andrewboylan@Andrews-MacBook-Pro-M3.local>
2024-12-05 11:54:19 -0500

    https working it seems

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,16 +1,16 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 const httpLink = createHttpLink({
-    uri: process.env.REACT_APP_API_URL,
-    fetchOptions: {
-      mode: 'cors'
-    },
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
-  
-  export const client = new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache()
-  });
+  uri: process.env.REACT_APP_API_URL,
+  fetchOptions: {
+    mode: 'cors'
+  },
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+export const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
